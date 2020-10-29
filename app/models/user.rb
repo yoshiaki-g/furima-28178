3e1434ea5ac0429c8b62c6c89b10f8aa1dce5cfa
yoshiaki-g <coyote.g.aki@gmail.com>
2020-10-29 15:34:25 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+         with_options presence: true do
+          validates :password, format: { with: VALID_PASSWORD_REGEX }
+          validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input Full-width characters."}
+          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input Full-width characters."}
+          validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input Full-width katakana characters."}
+          validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input Full-width katakana characters."}
+        end
 end
