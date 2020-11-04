d8745c0b55eae4c20da1fb73ad011338e28a515e
yoshiaki-g <coyote.g.aki@gmail.com>
2020-11-04 13:26:19 +0900

    ユーザー管理修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   has_many :items
   has_many :purchase_histories
+  VALID_ZENKAKU = /\A[ぁ-んァ-ン一-龥]/
+  VALID_ZENKAKU_KANA = /\A[ァ-ヶー－]+\z/
   with_options presence: true do
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "Include both letters and numbers"}
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-    validates :name ,:birthday, presence: true
+    validates :family_name, format: { with: VALID_ZENKAKU, message: "Full-width characters"}
+    validates :first_name, format: { with: VALID_ZENKAKU, message: "Full-width characters"}
+    validates :family_name_kana, format: { with: VALID_ZENKAKU_KANA, message: "Full-width katakana characters"}
+    validates :first_name_kana, format: { with: VALID_ZENKAKU_KANA, message: "Full-width katakana characters"}
+    validates :name ,:birthday
   end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
