7d58befe1bd0ee88f6227f19d2cd06da0b3f0f17
yoshiaki-g <coyote.g.aki@gmail.com>
2020-11-05 17:09:23 +0900

    ログインしているユーザーだけが、出品ページへ遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,17 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
-  # def new
-  #   @item = Item.new
-  # end
+  def new
+    @item = Item.new
+  end
 
-  # # def show
-  # # end
+  def show
+  end
 
-  # # def edit
-  # # end
+  def edit
+  end
 
   # def create
   #   @user = User.create(item_params)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_history
+
+  with_options presence: true do
+  end
 end
