cb3c7c54e0b1f501c2a80bfd5d8bfadbe5b4f000
yoshiaki-g <coyote.g.aki@gmail.com>
2020-11-10 15:48:54 +0900

    商品出品機能修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.valid?
        @item.save
        redirect_to root_path
