b54763a3a9636f42f232102bdcc1638c829fc300
yoshiaki-g <coyote.g.aki@gmail.com>
2020-11-12 20:07:30 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show]
+
   def index
     @items = Item.all
   end
@@ -8,10 +10,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to item_path
+   else
+      render 'edit'
+   end
   end
 
   def create
@@ -27,6 +39,10 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :value, :image, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :delivery_date_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
